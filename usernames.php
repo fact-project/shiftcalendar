49e94b65902f127cda3e6e321ba167487ecd0592
Dominik Neise <neised@phys.ethz.ch>
2016-05-29 20:25:39 +0200

    reading usernames from DB

---
+++ usernames.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once("/home/dneise/factread_db.php");
+// List of events
+ $json = array();
+
+ // Query that retrieves events
+ $requete = "SELECT username FROM logbook.users ORDER BY username";
+
+ // connection to the database
+ try {
+ $bdd = new PDO('mysql:host=localhost;dbname=factdata', $user, $pass);
+ } catch(Exception $e) {
+  exit('Unable to connect to database.');
+ }
+ // Execute the query
+ $resultat = $bdd->query($requete) or die(print_r($bdd->errorInfo()));
+
+ // sending the encoded result to success page
+ echo json_encode($resultat->fetchAll(PDO::FETCH_ASSOC));
+
+?>
