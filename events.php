72f21024be8c3ed3300f56557f9ab226c20d8913
Dominik Neise <neised@phys.ethz.ch>
2017-02-21 07:59:52 +0100

    remove useless comments; rename vars; tabs to spaces

--- events.php
+++ events.php
@@ -1,22 +1,15 @@
 <?php
+    require_once("/home/dneise/sandbox_db.php");
+    $json = array();
 
-require_once("/home/dneise/sandbox_db.php");
-// List of events
- $json = array();
+    $request = "SELECT * FROM shift ORDER BY id";
 
- // Query that retrieves events
- $requete = "SELECT * FROM shift ORDER BY id";
+    try {
+        $db = new PDO('mysql:host=localhost;dbname=sandbox', $user, $pass);
+    } catch(Exception $e) {
+        exit('Unable to connect to database.');
+    }
 
- // connection to the database
- try {
- $bdd = new PDO('mysql:host=localhost;dbname=sandbox', $user, $pass);
- } catch(Exception $e) {
-  exit('Unable to connect to database.');
- }
- // Execute the query
- $resultat = $bdd->query($requete) or die(print_r($bdd->errorInfo()));
-
- // sending the encoded result to success page
- echo json_encode($resultat->fetchAll(PDO::FETCH_ASSOC));
-
+    $result = $db->query($request) or die(print_r($db->errorInfo()));
+    echo json_encode($result->fetchAll(PDO::FETCH_ASSOC));
 ?>
