cac86e0468cfdcdad4a252770a9361979268ce01
Dominik Neise <neised@phys.ethz.ch>
2017-02-21 08:13:17 +0100

    indent; tab2space; rename vars;

--- delete_event.php
+++ delete_event.php
@@ -1,23 +1,19 @@
 <?php
+    require_once("/home/dneise/sandbox_db.php");
+    require_once('login.php');
 
-require_once("/home/dneise/sandbox_db.php");
-require_once('login.php');
+    $id = $_POST['id'];
+    $title = $_POST['title'];
+    $start = $_POST['start'];
+    $end = $_POST['end'];
 
-/* Values received via ajax */
-$id = $_POST['id'];
-$title = $_POST['title'];
-$start = $_POST['start'];
-$end = $_POST['end'];
+    try {
+        $db = new PDO('mysql:host=localhost;dbname=sandbox', $user, $pass);
+    } catch(Exception $e) {
+        exit('Unable to connect to database.');
+    }
 
-// connection to the database
-try {
- $bdd = new PDO('mysql:host=localhost;dbname=sandbox', $user, $pass);
- } catch(Exception $e) {
-exit('Unable to connect to database.');
-}
- // update the records
- $sql = "DELETE from shift WHERE id=".$id;
- $q = $bdd->prepare($sql);
- $q->execute();
+    $sql = "DELETE from shift WHERE id=".$id;
+    $query = $db->prepare($sql);
+    $query->execute();
 ?>
-
