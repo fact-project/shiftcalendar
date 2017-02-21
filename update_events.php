67faa178cc02d54f17cb19e5bdc5d1cb965e6802
Dominik Neise <neised@phys.ethz.ch>
2017-02-21 08:08:45 +0100

    indent; tab2space; rename vars;

--- update_events.php
+++ update_events.php
@@ -1,23 +1,19 @@
 <?php
+	require_once("/home/dneise/sandbox_db.php");
+	require_once('login.php');
 
-require_once("/home/dneise/sandbox_db.php");
-require_once('login.php');
+	$id = $_POST['id'];
+	$title = $_POST['title'];
+	$start = $_POST['start'];
+	$end = $_POST['end'];
 
-/* Values received via ajax */
-$id = $_POST['id'];
-$title = $_POST['title'];
-$start = $_POST['start'];
-$end = $_POST['end'];
+	try {
+		$db = new PDO('mysql:host=localhost;dbname=sandbox', $user, $pass);
+	} catch(Exception $e) {
+		exit('Unable to connect to database.');
+	}
 
-// connection to the database
-try {
- $bdd = new PDO('mysql:host=localhost;dbname=sandbox', $user, $pass);
- } catch(Exception $e) {
-exit('Unable to connect to database.');
-}
- // update the records
-$sql = "UPDATE shift SET title=?, start=?, end=? WHERE id=?";
-$q = $bdd->prepare($sql);
-$q->execute(array($title,$start,$end,$id));
+	$sql = "UPDATE shift SET title=?, start=?, end=? WHERE id=?";
+	$query = $db->prepare($sql);
+	$query->execute(array($title,$start,$end,$id));
 ?>
-
