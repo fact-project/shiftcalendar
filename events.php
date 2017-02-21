75dd34deda4c4491805146663900f1bff479dbc8
Dominik Neise <neised@phys.ethz.ch>
2017-02-21 14:56:06 +0000

    adjust events to the new DB table structure

--- events.php
+++ events.php
@@ -2,8 +2,35 @@
     require_once("db.php");
     $json = array();
 
-    $request = "SELECT * FROM shift ORDER BY id";
+    $db = create_db('factdata');
+    $request = "SELECT username, uid FROM logbook.users";
+    $result = $db->query($request) or die(print_r($db->errorInfo()));
+    $user2id = array();
+    $uid2name = array();
+    foreach($result->fetchAll(PDO::FETCH_ASSOC) as $u)
+    {
+        $user2id[$u['username']] = $u['uid'];
+        $uid2name[$u['uid']] = $u['username'];
+    }
+   
+    $request = "SELECT calendarentry.id, calendarentry.user_id, start, end, name as role from calendarentry join role on (calendarentry.role_id = role.id)";
     $db = create_db('sandbox');
     $result = $db->query($request) or die(print_r($db->errorInfo()));
-    echo json_encode($result->fetchAll(PDO::FETCH_ASSOC));
+    $calendarentries = $result->fetchAll(PDO::FETCH_ASSOC);
+
+    $entries = array();
+    foreach($calendarentries as $c)
+    {
+        $e = [
+            "id" => $c['id'],
+            "title" => $uid2name[$c['user_id']],
+            "start" => $c['start'],
+            "end" => $c['end'],
+            "url" => "",
+            "allDay" => 0,
+            "role" => $c['role']
+        ];
+        $entries[] = $e;
+    }
+    echo json_encode($entries);
 ?>
