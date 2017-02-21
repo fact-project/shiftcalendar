8c5e17b061dd786f4734e32a3e8b06644dda6a3d
Dominik Neise <neised@phys.ethz.ch>
2017-02-21 10:05:42 +0100

    use create_db(); tabs2spaces

--- update_events.php
+++ update_events.php
@@ -1,19 +1,14 @@
 <?php
-	require_once("/home/dneise/sandbox_db.php");
-	require_once('login.php');
+    require_once("db.php");
+    require_once('login.php');
 
-	$id = $_POST['id'];
-	$title = $_POST['title'];
-	$start = $_POST['start'];
-	$end = $_POST['end'];
+    $id = $_POST['id'];
+    $title = $_POST['title'];
+    $start = $_POST['start'];
+    $end = $_POST['end'];
 
-	try {
-		$db = new PDO('mysql:host=localhost;dbname=sandbox', $user, $pass);
-	} catch(Exception $e) {
-		exit('Unable to connect to database.');
-	}
-
-	$sql = "UPDATE shift SET title=?, start=?, end=? WHERE id=?";
-	$query = $db->prepare($sql);
-	$query->execute(array($title,$start,$end,$id));
+    $db = create_db('sandbox');
+    $sql = "UPDATE shift SET title=?, start=?, end=? WHERE id=?";
+    $query = $db->prepare($sql);
+    $query->execute(array($title, $start, $end, $id));
 ?>
--- usernames.php
+++ usernames.php
@@ -1,16 +1,11 @@
 <?php
-	require_once("/home/dneise/factread_db.php");
+	require_once("db.php");
 	require_once('login.php');
 
 	$json = array();
 	$query = "SELECT username FROM logbook.users ORDER BY username";
 
-	try {
-	 	$db = new PDO('mysql:host=10.0.100.21;dbname=factdata', $user, $pass);
-	} catch(Exception $e) {
-	  	exit('Unable to connect to database.');
-	}
+	$db = create_db('factdata')
 	$result = $db->query($query) or die(print_r($db->errorInfo()));
-
 	echo json_encode($result->fetchAll(PDO::FETCH_ASSOC));
 ?>
