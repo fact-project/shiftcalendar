5e5d9d46628aa6280d50c771fb68abd770c2aa44
Dominik Neise <neised@phys.ethz.ch>
2017-02-21 08:10:56 +0100

    indent; tab2space; rename vars;

--- usernames.php
+++ usernames.php
@@ -1,24 +1,16 @@
 <?php
+	require_once("/home/dneise/factread_db.php");
+	require_once('login.php');
 
-require_once("/home/dneise/factread_db.php");
-require_once('login.php');
+	$json = array();
+	$query = "SELECT username FROM logbook.users ORDER BY username";
 
-// List of events
- $json = array();
+	try {
+	 	$db = new PDO('mysql:host=10.0.100.21;dbname=factdata', $user, $pass);
+	} catch(Exception $e) {
+	  	exit('Unable to connect to database.');
+	}
+	$result = $db->query($query) or die(print_r($db->errorInfo()));
 
- // Query that retrieves events
- $requete = "SELECT username FROM logbook.users ORDER BY username";
-
- // connection to the database
- try {
- $bdd = new PDO('mysql:host=10.0.100.21;dbname=factdata', $user, $pass);
- } catch(Exception $e) {
-  exit('Unable to connect to database.');
- }
- // Execute the query
- $resultat = $bdd->query($requete) or die(print_r($bdd->errorInfo()));
-
- // sending the encoded result to success page
- echo json_encode($resultat->fetchAll(PDO::FETCH_ASSOC));
-
+	echo json_encode($result->fetchAll(PDO::FETCH_ASSOC));
 ?>
