7bade4043360b9043b773d29255bbdad4485a012
Dominik Neise <neised@phys.ethz.ch>
2017-02-21 16:12:17 +0100

    add color to roles

--- shiftcalendar/roles.py
+++ shiftcalendar/roles.py
@@ -2,20 +2,32 @@ from .models import Role
 
 LEGACY_SHIFTER = Role(
 	name='legacy_shifter',
-	title='the pre 2017 standard shifter')
+	title='the pre 2017 standard shifter',
+	color="DarkRed"
+	)
 LEGACY_EXPERT = Role(
 	name='legacy_expert',
-	title='the pre 2017 standard expert')
+	title='the pre 2017 standard expert',
+	color="Green",
+	)
 FALLBACK_SHIFTER = Role(
 	name='fallback_shifter',
-	title="gets all calls in case the real real shifter doesn't react")
+	title="gets all calls in case the real real shifter doesn't react",
+	color="Red",
+	)
 SHIFTER_ON_CALL = Role(
 	name='shifter_on_call',
-	title='recieves calls in the middle of the night')
+	title='recieves calls in the middle of the night',
+	color="LightSeaGreen",
+	)
 STARTER = Role(
 	name='starter',
-	title='performs the startup')
+	title='performs the startup',
+	color='DarkOrange',
+	)
 STOPPER = Role(
 	name='stopper',
-	title='performs the shutdown')
+	title='performs the shutdown'
+	color='PaleGreen',
+	)
 
