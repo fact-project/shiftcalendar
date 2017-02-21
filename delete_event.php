1a0e05a8a0403fcd9817e5f444cb6d9933f03126
Dominik Neise <neised@phys.ethz.ch>
2017-02-21 10:03:40 +0100

    user create_db()

--- delete_event.php
+++ delete_event.php
@@ -1,5 +1,5 @@
 <?php
-    require_once("/home/dneise/sandbox_db.php");
+    require_once("db.php");
     require_once('login.php');
 
     $id = $_POST['id'];
@@ -7,12 +7,7 @@
     $start = $_POST['start'];
     $end = $_POST['end'];
 
-    try {
-        $db = new PDO('mysql:host=localhost;dbname=sandbox', $user, $pass);
-    } catch(Exception $e) {
-        exit('Unable to connect to database.');
-    }
-
+    $db = create_db('sandbox');
     $sql = "DELETE from shift WHERE id=".$id;
     $query = $db->prepare($sql);
     $query->execute();
