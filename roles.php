ae9b6cc51ea264798262f4aa16c24b0b0ced5b93
Dominik Neise <neised@phys.ethz.ch>
2017-02-21 15:18:38 +0000

    fix typo

--- roles.php
+++ roles.php
@@ -2,7 +2,7 @@
     require_once("db.php");
 
     $json = array();
-    $query = "SELECT * FROM roles";
+    $query = "SELECT * FROM role";
     $db = create_db('sandbox');
     $result = $db->query($query) or die(print_r($db->errorInfo()));
     echo json_encode($result->fetchAll(PDO::FETCH_ASSOC));
