201513e2168491b0c721179c9981e52985a552fb
Dominik Neise <neised@phys.ethz.ch>
2017-02-22 09:26:36 +0100

    adjust for update event

--- update_events.php
+++ update_events.php
@@ -3,12 +3,11 @@
     require_once('login.php');
 
     $id = $_POST['id'];
-    $title = $_POST['title'];
     $start = $_POST['start'];
     $end = $_POST['end'];
 
     $db = create_db('sandbox');
-    $sql = "UPDATE shift SET title=?, start=?, end=? WHERE id=?";
+    $sql = "UPDATE calendarentry SET start=?, end=? WHERE id=?";
     $query = $db->prepare($sql);
-    $query->execute(array($title, $start, $end, $id));
+    $query->execute(array($start, $end, $id));
 ?>
