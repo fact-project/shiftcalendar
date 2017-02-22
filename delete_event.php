2318a304b07cd270f6ec689e9367cea6b2dac944
Dominik Neise <neised@phys.ethz.ch>
2017-02-22 09:00:23 +0100

    adjust delete_event

--- delete_event.php
+++ delete_event.php
@@ -1,14 +1,9 @@
 <?php
     require_once("db.php");
     require_once('login.php');
-
     $id = $_POST['id'];
-    $title = $_POST['title'];
-    $start = $_POST['start'];
-    $end = $_POST['end'];
-
     $db = create_db('sandbox');
-    $sql = "DELETE from shift WHERE id=".$id;
+    $sql = "DELETE from calendarentry WHERE id=".$id;
     $query = $db->prepare($sql);
     $query->execute();
 ?>
