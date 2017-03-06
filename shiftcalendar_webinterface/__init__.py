b69857d14898ccc3f71f8f9bb245d243a74858f1
Dominik Neise <neised@phys.ethz.ch>
2017-03-06 17:48:59 +0100

    switch auth on again

--- shiftcalendar_webinterface/__init__.py
+++ shiftcalendar_webinterface/__init__.py
@@ -2,10 +2,13 @@ import os
 from datetime import datetime, timedelta
 import json
 
+from .authentication import login_manager, basic_auth, authenticate_user
+
+
 from flask import Flask, jsonify, render_template, redirect, request, flash, Markup
-# from flask_login import login_user, login_required, logout_user
-# from flask_socketio import SocketIO
-# from flask_login import current_user
+from flask_login import login_user, login_required, logout_user
+from flask_socketio import SocketIO
+from flask_login import current_user
 
 from twilio.rest import TwilioRestClient
 from twilio.exceptions import TwilioException
@@ -15,20 +18,26 @@ from shiftcalendar.database import connect_databases
 from shiftcalendar.models import Role, CalendarEntry, LegacyCalendarEntry
 from shiftcalendar.logbook_models import Users
 
+
+
 app = Flask(__name__)
+app.secret_key = b'\xf8P\x04\xd8Nk4ve\x8d\xd8\xe3\x9f\xccx`\xf3\x88\xb6\rQ\xf4\xc7\x8a'
+login_manager.init_app(app)
+
+user2id = {}
+uid2name = {}
 
 @app.before_first_request
 def init_db():
-    pass
-    """
-    database.connect()
-    database.create_tables([Alert], safe=True)
-    database.close()
-    """
+    connect_databases()
+    q = Users.select(Users.username, Users.uid)
+    for d in q:
+        user2id[d.username] = d.uid
+        uid2name[d.uid] = d.username
 
 @app.before_request
 def _db_connect():
-    connect_databases()
+    pass
 
 @app.teardown_request
 def _db_close(exc):
@@ -43,16 +52,61 @@ def index():
 
 
 @app.route('/roles')
+@login_required
 def get_roles():
     return json.dumps(list(Role.select().dicts()))
 
 @app.route('/usernames')
+@login_required
 def get_usernames():
     q = Users.select(Users.username, Users.uid).dicts()
     l = [d for d in q]
     for d in l:
         d['user_id'] = d.pop('uid')
     return json.dumps(l)
+
+@app.route('/events', methods=["GET"])
+#@login_required
+def get_events():
+    def str2date(s):
+        return datetime.strptime(s, '%Y-%m-%d')
+
+    start = request.args.get('start', default="2011-01-01", type=str2date)
+    end = request.args.get('end', default="2100-01-01", type=str2date)
+    l = list(CalendarEntry
+        .select(Role, CalendarEntry)
+        .join(Role)
+        .where((start < CalendarEntry.start) & (end > CalendarEntry.end))
+        .dicts())
+    for d in l:
+        d['url'] = ''
+        d['allDay'] = 0
+        d['title'] = uid2name[d['user_id']]
+    return jsonify(l)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+
+    if request.method == 'GET':
+        return redirect('/')
+
+    username = request.form['username']
+    password = request.form['password']
+
+    user = authenticate_user(username, password)
+
+    if user is not None:
+        login_user(user)
+        return redirect('/')
+    else:
+        flash('Wrong username/password', 'alert-danger')
+        return redirect('/')
+
+
+@app.route('/logout', methods=['GET', 'POST'])
+def logout():
+    logout_user()
+    return redirect('/')
 
 """
 def remove_alert(uuid):
@@ -145,29 +199,6 @@ def get_alert(uuid):
     except Alert.DoesNotExist:
         return jsonify(status='No such alert'), 404
 
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'GET':
-        return redirect('/')
-
-    username = request.form['username']
-    password = request.form['password']
-
-    user = authenticate_user(username, password)
-
-    if user is not None:
-        login_user(user)
-        return redirect('/')
-    else:
-        flash('Wrong username/password', 'alert-danger')
-        return redirect('/')
-
-
-@app.route('/logout', methods=['GET', 'POST'])
-def logout():
-    logout_user()
-    return redirect('/')
-
-
 """
+
+
